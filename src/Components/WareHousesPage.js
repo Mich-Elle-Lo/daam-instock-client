93f260629c01722600e810956a490b4f63fb93c8
Michelle Lo <michellelo@live.ca>
2024-02-05 21:03:05 -0500

    add the map, will be ready once we can get data, still need to work on styling

--- src/Components/WareHousesPage.js
+++ src/Components/WareHousesPage.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WareHousesPage.scss";
 import searchIcon from "../Assets/Icons/search-24px.svg";
 import sortIcon from "../Assets/Icons/sort-24px.svg";
+import trashIcon from "../Assets/Icons/delete_outline-24px.svg";
+import editIcon from "../Assets/Icons/edit-24px.svg";
 
 export default function WareHousesPage() {
+  const [warehouses, setWarehouses] = useState([]);
+
   return (
     <section className="warehouses">
       <div className="warehouses__wrapper">
@@ -59,6 +63,53 @@ export default function WareHousesPage() {
           />
         </div>
         <div className="warehouses__box">ACTIONS</div>
+      </div>
+      <div className="warehouses__list">
+        {warehouses.map((warehouse) => (
+          <div key={warehouse.id} className="warehouses__card">
+            <div className="warehouses__details">
+              <div className="warehouses__mobilebox">
+                <div className="warehouses__infobox">
+                  <div className="warehouses__infotitle">WAREHOUSE</div>
+                  <div>{warehouse.name}</div>
+                </div>
+                <div className="warehouses__infobox">
+                  <div className="warehouses__infotitle">ADDRESS</div>
+                  <div>{warehouse.address}</div>
+                </div>
+              </div>
+              <div className="warehouses__mobilebox">
+                <div className="warehouses__infobox">
+                  <div className="warehouses__infotitle">CONTACT NAME</div>
+                  <div>{warehouse.contact_name}</div>
+                </div>
+                <div className="warehouses__infobox">
+                  <div className="warehouses__infotitle">
+                    CONTACT INFORMATION
+                  </div>
+                  <div>{warehouse.contact_phone}</div>
+                  <div>{warehouse.contact_email}</div>
+                </div>
+              </div>
+            </div>
+            <div className="warehouses__actions">
+              <div className="warehouses__trash">
+                <img
+                  className="warehouses__trashicon"
+                  src={trashIcon}
+                  alt="trash icon"
+                />
+              </div>
+              <div className="warehouses__edit">
+                <img
+                  className="warehouses__editicon"
+                  src={editIcon}
+                  alt="edit icon"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
