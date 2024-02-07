ea2e24c240fd5dbd1c911c2d25a1ff47b12a2b53
AnkitaDes <desai_ankita@outlook.com>
2024-02-07 17:14:10 -0500

    remove errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-
+import InventoriesPage from "./Components/D23UPE1-11/InventoriesPage";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<WareHousesPage />} />
+          {/* <Route path="/" element={<WareHousesPage />} /> */}
           <Route path="/inventories" element={<InventoriesPage />} />
         </Routes>
         <Footer />
