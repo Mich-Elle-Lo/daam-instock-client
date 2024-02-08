3a4dd92b79452a963b8067a87cbc2baf086da4c4
Anita Yawson <anitayawson@gmail.com>
2024-02-08 01:38:12 -0500

    add background color in app.scss

--- src/Components/EditWarehouse/EditWarehouse.js
+++ src/Components/EditWarehouse/EditWarehouse.js
@@ -4,38 +4,78 @@ import BackArrow from "../../Assets/Icons/arrow_back-24px.svg";
 export default function EditWarehouse() {
   return (
     <section className="edit-warehouse">
-      <div className="edit__title-container">
+      <div className="edit-warehouse__title-container">
         <img src={BackArrow} alt="Back Arrow" />
         <h1>Edit Warehouse</h1>
       </div>
       <form className="edit-warehouse__form">
-        <div className="edit__warehouse-details">
+        <div className="edit-warehouse__warehouse-details">
           <h3>Warehouse Details</h3>
           <label htmlFor="warehouseName">Warehouse Name</label>
-          <input type="text" id="warehouseName" name="warehouseName" />
+          <input
+            className="edit-warehouse__input"
+            type="text"
+            id="warehouseName"
+            name="warehouseName"
+          />
 
           <label htmlFor="streetAddress">Street Address</label>
-          <input type="text" id="streetAddress" name="streetAddress" />
+          <input
+            className="edit-warehouse__input"
+            type="text"
+            id="streetAddress"
+            name="streetAddress"
+          />
 
           <label htmlFor="city">City</label>
-          <input type="text" id="city" name="city" />
+          <input
+            className="edit-warehouse__input"
+            type="text"
+            id="city"
+            name="city"
+          />
 
           <label htmlFor="country">Country</label>
-          <input type="text" id="country" name="country" />
+          <input
+            className="edit-warehouse__input"
+            type="text"
+            id="country"
+            name="country"
+          />
         </div>
-        <div className="edit__contact-details">
+        <div className="edit-warehouse__contact-details">
           <h3>Contact Details</h3>
           <label htmlFor="contactName">Contact Name</label>
-          <input type="text" id="contactName" name="contactName" />
+          <input
+            className="edit-warehouse__input"
+            type="text"
+            id="contactName"
+            name="contactName"
+          />
 
           <label htmlFor="position">Position</label>
-          <input type="text" id="position" name="position" />
+          <input
+            className="edit-warehouse__input"
+            type="text"
+            id="position"
+            name="position"
+          />
 
           <label htmlFor="phone">Phone Number</label>
-          <input type="text" id="phone" name="phone" />
+          <input
+            className="edit-warehouse__input"
+            type="text"
+            id="phone"
+            name="phone"
+          />
 
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" />
+          <input
+            className="edit-warehouse__input"
+            type="text"
+            id="email"
+            name="email"
+          />
         </div>
         <div>
           <button type="button">Cancel</button>
