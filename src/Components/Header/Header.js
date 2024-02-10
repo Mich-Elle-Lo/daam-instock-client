0d789f3327a293a05a0502220b9d8e186da6d53e
Michelle Lo <michellelo@live.ca>
2024-02-10 09:57:21 -0500

    check

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,10 +5,21 @@ import { Link, useLocation } from "react-router-dom";
 export default function Header() {
   const location = useLocation();
 
-  const getClassName = (path) => {
-    return location.pathname === path
-      ? "header__nav-item header__nav-item--active"
-      : "header__nav-item";
+  const isActiveExact = (path) => {
+    return location.pathname === path;
+  };
+
+  const isActiveIncludes = (keyword) => {
+    return location.pathname.includes(keyword);
+  };
+
+  const getClassName = (exactPath, keyword) => {
+    if (exactPath && isActiveExact(exactPath)) {
+      return "header__nav-item header__nav-item--active";
+    } else if (keyword && isActiveIncludes(keyword)) {
+      return "header__nav-item header__nav-item--active";
+    }
+    return "header__nav-item";
   };
 
   return (
@@ -20,12 +31,12 @@ export default function Header() {
       </div>
       <nav className="header__nav">
         <ul className="header__nav-list">
-          <li className={getClassName("/")}>
+          <li className={getClassName("/", "warehouse")}>
             <Link className="header__nav-link" to="/">
               Warehouses
             </Link>
           </li>
-          <li className={getClassName("/inventories")}>
+          <li className={getClassName(null, "inventories")}>
             <Link className="header__nav-link" to="/inventories">
               Inventory
             </Link>
